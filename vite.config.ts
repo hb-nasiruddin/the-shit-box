7eec6175cf389e5f2a67f7d109339c9bff938b32
hb-nasiruddin <nasiruddin.saiyed@hiddenbrains.in>
2024-12-02 19:21:57 +0530

    v0.0.1 - Initial Build for static subpath

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,32 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [
+        react(),
+        legacy()
+      ],
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+      }
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [
+        react(),
+        legacy()
+      ],
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+      },
+      base: "/the-shit-box/",
+    }
   }
-})
+});
