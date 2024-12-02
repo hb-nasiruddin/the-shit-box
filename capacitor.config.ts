7464c7f42d528ed14c4dba0614928f3d4b71cb1d
hb-nasiruddin <hb.nasiruddinsaiyed@hiddenbrains.in>
2024-12-02 11:07:36 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'The Shit Box',
+  webDir: 'dist'
+};
+
+export default config;
